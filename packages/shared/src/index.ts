205b0305cd763dc7439718e1fe1bdc1c97bddb57
pwc <pengwencheng@dlz-tech.com>
2024-11-22 10:36:59 +0800

    chore: init

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,9 @@
+export const ErrorProp = {
+  BadRequest: 'BAD_REQUEST',
+  Unauthorized: 'UNAUTHORIZED',
+  Forbidden: 'FORBIDDEN',
+  NotFound: 'NOT_FOUND',
+  Conflict: 'CONFLICT',
+  InternalServerError: 'INTERNAL_SERVER_ERROR',
+  NotImplemented: 'NOT_IMPLEMENTED',
+};
